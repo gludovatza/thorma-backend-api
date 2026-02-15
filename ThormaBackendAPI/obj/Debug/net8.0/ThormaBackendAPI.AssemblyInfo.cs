31ce3f605c547937a2fde5243481c9edb73005d3
gludovatza <attila@gludovatz.hu>
2026-02-15 14:41:52 +0100

    MySQL és EF csomagok telepítése

--- ThormaBackendAPI/obj/Debug/net8.0/ThormaBackendAPI.AssemblyInfo.cs
+++ ThormaBackendAPI/obj/Debug/net8.0/ThormaBackendAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThormaBackendAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4482276e308edc2c74d472915145c45a87e9c1c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThormaBackendAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThormaBackendAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
