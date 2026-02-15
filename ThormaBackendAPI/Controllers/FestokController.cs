5f2b917a240413e3cacccdbc5fe07748f33688a0
gludovatza <attila@gludovatz.hu>
2026-02-15 15:11:17 +0100

    CRUD vezérlők

---
+++ ThormaBackendAPI/Controllers/FestokController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ThormaBackendAPI.Data;
+using ThormaBackendAPI.Models;
+
+namespace ThormaBackendAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FestokController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public FestokController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Festok
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Festo>>> GetFestok()
+        {
+            return await _context.Festok.ToListAsync();
+        }
+
+        // GET: api/Festok/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Festo>> GetFesto(int id)
+        {
+            var festo = await _context.Festok.FindAsync(id);
+
+            if (festo == null)
+            {
+                return NotFound();
+            }
+
+            return festo;
+        }
+
+        // PUT: api/Festok/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFesto(int id, Festo festo)
+        {
+            if (id != festo.Azon)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(festo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FestoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Festok
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Festo>> PostFesto(Festo festo)
+        {
+            _context.Festok.Add(festo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFesto", new { id = festo.Azon }, festo);
+        }
+
+        // DELETE: api/Festok/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFesto(int id)
+        {
+            var festo = await _context.Festok.FindAsync(id);
+            if (festo == null)
+            {
+                return NotFound();
+            }
+
+            _context.Festok.Remove(festo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool FestoExists(int id)
+        {
+            return _context.Festok.Any(e => e.Azon == id);
+        }
+    }
+}
---
+++ ThormaBackendAPI/Controllers/KepController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ThormaBackendAPI.Data;
+using ThormaBackendAPI.Models;
+
+namespace ThormaBackendAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KepController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public KepController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Kep
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Kep>>> GetKepek()
+        {
+            return await _context.Kepek.ToListAsync();
+        }
+
+        // GET: api/Kep/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Kep>> GetKep(string id)
+        {
+            var kep = await _context.Kepek.FindAsync(id);
+
+            if (kep == null)
+            {
+                return NotFound();
+            }
+
+            return kep;
+        }
+
+        // PUT: api/Kep/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutKep(string id, Kep kep)
+        {
+            if (id != kep.Leltar)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(kep).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!KepExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Kep
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Kep>> PostKep(Kep kep)
+        {
+            _context.Kepek.Add(kep);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (KepExists(kep.Leltar))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetKep", new { id = kep.Leltar }, kep);
+        }
+
+        // DELETE: api/Kep/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteKep(string id)
+        {
+            var kep = await _context.Kepek.FindAsync(id);
+            if (kep == null)
+            {
+                return NotFound();
+            }
+
+            _context.Kepek.Remove(kep);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool KepExists(string id)
+        {
+            return _context.Kepek.Any(e => e.Leltar == id);
+        }
+    }
+}
