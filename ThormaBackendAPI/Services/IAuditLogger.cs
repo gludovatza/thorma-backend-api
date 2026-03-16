e582d657148c48b2536b96db6e0b963f05d65c60
gludovatza <attila@gludovatz.hu>
2026-03-16 13:45:46 +0100

    AuditLogger Service létrehozása

---
+++ ThormaBackendAPI/Services/IAuditLogger.cs
@@ -0,0 +1,14 @@
+﻿namespace ThormaBackendAPI.Services
+{
+    public interface IAuditLogger
+    {
+        Task LogChangeAsync<T>(
+            string userId,
+            string userEmail,
+            string entityType,
+            string entityId,
+            string action,
+            T? oldValue,
+            T? newValue);
+    }
+}
